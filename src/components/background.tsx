51b46ce656fd35b453f65e9c82e6522e7a11b5ca
zackdesu <zarchxxx@gmail.com>
2023-07-07 09:43:20 +0700

    background

---
+++ src/components/background.tsx
@@ -0,0 +1,18 @@
+import "./background.css";
+
+export default function Background() {
+  return (
+    <div className="bubbles">
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+    </div>
+  );
+}
