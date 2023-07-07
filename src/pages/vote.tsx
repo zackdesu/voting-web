8210f48d0bb29dad9516258b0c7422a138e1caea
zackdesu <zarchxxx@gmail.com>
2023-07-07 09:43:47 +0700

    ref forward

--- src/pages/vote.tsx
+++ src/pages/vote.tsx
@@ -1,20 +1,25 @@
+import { forwardRef, ForwardedRef } from "react";
 import Card from "../components/card";
 
-export default function Vote() {
+const Vote = forwardRef((_props, ref: ForwardedRef<HTMLDivElement>) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="text-center">
-        <h1>Vote</h1>
-        <p>- Dukunglah pilihanmu -</p>
+    <>
+      <div className="flex flex-col items-center relative p-5" ref={ref}>
+        <div className="text-center">
+          <h1>Vote</h1>
+          <p>- Dukunglah pilihanmu -</p>
+        </div>
+        <div className="mt-10 flex flex-wrap justify-center lg:w-[738px] lg:h-[544px] sm:w-[600px] sm:h-[480px]">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
-      <div className="mt-10 flex flex-wrap justify-center lg:w-[738px] lg:h-[544px] sm:w-[600px] sm:h-[480px]">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </div>
+    </>
   );
-}
+});
+
+export default Vote;
