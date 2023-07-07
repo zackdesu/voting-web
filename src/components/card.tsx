c91bec0fd5da751acd7a134ed309c934b1e417b6
zackdesu <zarchxxx@gmail.com>
2023-07-07 10:13:33 +0700

    add parameter src and name

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,14 @@
-export default function Card() {
+export default function Card({ src, name }: { src?: string; name: string }) {
   return (
     <div className="mx-2 my-2">
       <div className="lg:w-48 lg:h-64 sm:w-[10.5rem] sm:h-[14rem] w-[7.5rem] h-[10rem] flex flex-col items-center bg-blue-950 p-2 rounded justify-between sm:pb-5">
-        <div className="bg-black w-full h-1/2" />
+        <div className="w-full h-1/2">
+          <img src={src} className="object-cover w-full h-full" alt={name} />
+        </div>
         <div className="flex flex-col items-center h-1/2 w-full justify-evenly">
           <div className="text-center">
             <p>
-              <b>Alok</b>
+              <b>{name}</b>
             </p>
             <p>Visi & Misi</p>
           </div>
