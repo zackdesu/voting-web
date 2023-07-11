08db3dc94e9b0646633824d48fa8e2a24db1d37b
zackdesu <zarchxxx@gmail.com>
2023-07-11 16:06:04 +0700

    add 2 modal. confirm & detail. visi & misi array, useState, visi & misi onClick show modals

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { PiWarningCircleBold } from "react-icons/pi";
-import { AiOutlineClose } from "react-icons/ai";
 import { api } from "./api";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
@@ -9,19 +7,24 @@ export default function Card({
   src,
   name,
   id,
+  visi,
+  misi,
 }: {
   src?: string;
   name: string;
   id: string;
+  visi?: Array<string>;
+  misi?: Array<string>;
 }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isConfirmOpen, setisConfirmOpen] = useState(false);
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleConfirm = (id: string) => {
     api
       .post(`/selection/${id}`)
       .then((res) => {
-        setIsOpen(false);
+        setisConfirmOpen(false);
         return toast.success(res.data.message);
       })
       .catch((res) => {
@@ -33,36 +36,102 @@ export default function Card({
       });
   };
 
-  const Modal = () => {
+  const ConfirmModal = () => {
     return (
       <>
         <div className="bg-[rgba(0,0,0,.5)] fixed top-0 right-0 left-0 bottom-0"></div>
-        <div className="fixed top-1/4 bottom-1/4 w-10/12 sm:w-3/4 md:w-1/2 lg:w-5/12 bg-yellow-300 rounded flex flex-col items-center justify-around z-20 p-5">
-          <button
-            className="absolute -right-3 -top-5 bg-yellow-400 ring-1 ring-blue-600 rounded-full p-2 text-blue-700 font-bold"
-            onClick={() => setIsOpen(false)}
-          >
-            <AiOutlineClose size={20} />
-          </button>
-          <div className="flex flex-col items-center">
-            <div>
-              <PiWarningCircleBold size={70} className="text-red-600 mb-3" />
+        <div className="fixed top-1/4 bottom-1/4 flex items-center justify-center lg:w-3/4">
+          <div className="h-1/2 lg:w-1/2 bg-yellow-50 rounded-lg flex flex-col justify-between z-20 py-3 md:py-2 px-3 shadow-lg max-sm:m-3">
+            <div className="flex flex-col">
+              <h4 className="text-blue-950 mt-2">
+                Apakah anda yakin dengan pilihan anda?
+              </h4>
+              <p className="text-blue-950 opacity-70 text-[.55rem] sm:text-[.6rem] md:text-xs mt-1">
+                Setelah anda memilih, anda tidak bisa memilih peserta yang
+                lainnya.
+              </p>
             </div>
-            <h3 className="text-blue-700 mt-3">Apakah kamu yakin?</h3>
+            <div className="flex justify-end mb-1">
+              <button
+                className="px-5 py-2 xl:mx-2 mx-1 bg-zinc-200 hover:bg-zinc-300 text-zinc-500 hover:text-zinc-600 rounded-full cursor-pointer shadow font-medium"
+                onClick={() => setisConfirmOpen(false)}
+              >
+                <p className="text-[.55rem] sm:text-[.6rem] md:text-xs">
+                  Tidak
+                </p>
+              </button>
+              <button
+                className="px-3 md:px-5 py-2 xl:mx-2 mx-1 bg-blue-700 hover:bg-blue-800 text-yellow-50 hover:text-yellow-100 rounded-full cursor-pointer shadow font-medium"
+                onClick={() => handleConfirm(id)}
+              >
+                <p className="text-[.55rem] sm:text-[.6rem] md:text-xs">
+                  Ya, saya yakin.
+                </p>
+              </button>
+            </div>
           </div>
-          <div>
-            <button
-              className="px-5 py-2 xl:mx-2 mx-1 bg-blue-700 hover:bg-blue-800 rounded cursor-pointer shadow font-medium"
-              onClick={() => handleConfirm(id)}
-            >
-              Ya
-            </button>
-            <button
-              className="px-5 py-2 xl:mx-2 mx-1 bg-blue-700 hover:bg-blue-800 rounded cursor-pointer shadow font-medium"
-              onClick={() => setIsOpen(false)}
-            >
-              Tidak
-            </button>
+        </div>
+      </>
+    );
+  };
+
+  const DetailModal = ({
+    name,
+    visi,
+    misi,
+  }: {
+    name: string;
+    visi?: Array<string>;
+    misi?: Array<string>;
+  }) => {
+    return (
+      <>
+        <div className="bg-[rgba(0,0,0,.5)] fixed top-0 right-0 left-0 bottom-0"></div>
+        <div className="fixed top-1/4 bottom-1/4 flex items-center justify-center w-3/4">
+          <div className="h-fit w-3/4 bg-yellow-50 rounded-lg flex flex-col justify-between z-20 py-3 md:py-2 px-3 shadow-lg max-sm:m-3">
+            <div className="flex flex-col">
+              <h4 className="text-blue-950 my-2 text-center">{name}</h4>
+              <div className="flex flex-col max-h-60 overflow-y-auto minimal">
+                <p className="text-center mb-2">- Visi -</p>
+                <ol
+                  type="1"
+                  start={1}
+                  className="text-blue-950 opacity-70 text-[.55rem] sm:text-[.6rem] md:text-xs my-1 list-decimal mx-auto"
+                >
+                  {visi
+                    ? visi?.map((e, i) => (
+                        <li className="cursor-default pl-10" key={i}>
+                          {e}
+                        </li>
+                      ))
+                    : "Tidak ada"}
+                </ol>
+                <p className="text-center mb-2">- Misi -</p>
+                <ol
+                  type="1"
+                  start={1}
+                  className="text-blue-950 opacity-70 text-[.55rem] sm:text-[.6rem] md:text-xs my-1 list-decimal mx-auto"
+                >
+                  {misi
+                    ? misi?.map((e, i) => (
+                        <li className="cursor-default pl-10" key={i}>
+                          {e}
+                        </li>
+                      ))
+                    : "Tidak ada"}
+                </ol>
+              </div>
+            </div>
+            <div className="flex justify-end mb-1">
+              <button
+                className="px-5 py-2 xl:mx-2 mx-1 bg-zinc-200 hover:bg-zinc-300 text-zinc-500 hover:text-zinc-600 rounded-full cursor-pointer shadow font-medium"
+                onClick={() => setIsDetailOpen(false)}
+              >
+                <p className="text-[.55rem] sm:text-[.6rem] md:text-xs">
+                  Tutup
+                </p>
+              </button>
+            </div>
           </div>
         </div>
       </>
@@ -71,8 +140,8 @@ export default function Card({
 
   return (
     <>
-      <div className="mx-2 my-2">
-        <div className="lg:w-48 lg:h-64 sm:w-[10.5rem] sm:h-[14rem] w-[7.5rem] h-[10rem] flex flex-col items-center bg-blue-950 p-2 rounded justify-between sm:pb-5">
+      <div className="m-2 max-[319px]:m-1">
+        <div className="lg:w-48 lg:h-64 sm:w-[10.5rem] sm:h-[14rem] w-[9rem] h-[12rem] flex flex-col items-center bg-blue-950 p-2 rounded justify-between sm:pb-5">
           <div className="w-full h-1/2">
             <img src={src} className="object-cover w-full h-full" alt={name} />
           </div>
@@ -81,14 +150,17 @@ export default function Card({
               <p>
                 <b>{name}</b>
               </p>
-              <p className="hover:underline underline-offset-1 cursor-pointer">
+              <p
+                className="hover:underline underline-offset-1 cursor-pointer"
+                onClick={() => setIsDetailOpen(true)}
+              >
                 Visi & Misi
               </p>
             </div>
             <div className="flex justify-center">
               <button
                 className="px-5 py-2 xl:mx-2 mx-1 bg-blue-700 hover:bg-blue-800 rounded cursor-pointer shadow font-medium"
-                onClick={() => setIsOpen(true)}
+                onClick={() => setisConfirmOpen(true)}
               >
                 <p>Pilih</p>
               </button>
@@ -96,7 +168,8 @@ export default function Card({
           </div>
         </div>
       </div>
-      {isOpen && <Modal />}
+      {isDetailOpen && <DetailModal name={name} visi={visi} misi={misi} />}
+      {isConfirmOpen && <ConfirmModal />}
     </>
   );
 }
